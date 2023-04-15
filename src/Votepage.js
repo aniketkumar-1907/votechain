a488f9cac89a22744e9d4f4a9344ceaaaf50e87f
Akshat <akshatdec27@gmail.com>
2023-04-16 02:26:32 +0530

    voting page rendered

---
+++ src/Votepage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer from "./Footer"
+import Header from './Header'
+
+export default function Votepage() {
+    
+  return (
+    
+    <div style={{height:'100vh'}}>
+      <Header/>
+      <p>hello</p>
+      
+    </div>
+
+  )
+}
+
