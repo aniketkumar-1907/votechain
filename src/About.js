a50f1ab5ab03e6a34c5e159007f8110a2ef710c9
siddharth-gautam-ug21-007-nitp <siddharthg.ug21.cs@nitp.ac.in>
2023-04-16 00:31:49 +0530

    Sid change

--- src/About.js
+++ src/About.js
@@ -2,19 +2,27 @@ import React from 'react'
 import Header from "./Header";
 const About = () => {
   return (
-    <div className="about">
+    <>
     <Header/>
+    <div className="about">
+    
      <p>
-     Blockchain technology, known for its secure, transparent, and tamper-proof capability popularised by its use in the crypto trade, is being studied in some places whether it can be used for voting in an election
+     Blockchain technology, known for its secure, transparent, and tamper-proof capability popularised by its use in the crypto trade, is being studied in some places whether it can be used for voting in an election.
+     <br/>
+     <br/>
+     <br/>
      <h3>Can Blockchain Make Voting More Transparent?</h3>
+     <br/>
      <p>Blockchain is a distributed database that is secure and tamper-proof. According to blockchain experts, this makes it an ideal technology for voting, as it would provide a transparent and secure way to record and count votes. "There have been several pilot projects using blockchain voting in India, and the results have been promising," says Lokesh Rao, co-founder and CEO, Trace Network Labs. 
 
 Rao further noted that one of the biggest advantages of blockchain voting is that it would help reduce the incidence of voter fraud. "Currently, there are many ways in which votes can be tampered with or manipulated. With blockchain, each vote would be recorded securely and could not be changed. This would make it much more difficult for anyone to rig the system," he said</p>
 
      </p>
+     <br/>
      <h3>Are There Other Advantages?</h3>
+     <br/>
      <p>Transparency through blockchain voting may also help increase the turnout. With blockchain, people would be able to see that their vote is being counted accurately and that their voice is being heard. This could lead to more people taking part in the democratic process.
-
+<br/>
 "Blockchain provides a secure and transparent way to store data, which is essential for ensuring the accuracy and integrity of voting systems. Furthermore, blockchain can help to improve voter turnout by making it easier for people to register to vote and to track their votes," says Sagar Vishnoi, Independent political campaigner and strategist.
 
 Experts believe that another potential use case for blockchain voting is the auditing of results. This could be done by checking the blockchain to see that all the votes have been correctly recorded and that the correct candidate has been chosen. This would be a quick and easy way to ensure the accuracy of the results. 
@@ -22,6 +30,7 @@ In India, blockchain-based voting has already been tested for Telangana’s muni
 
 Experts believe that blockchain voting is still in its early stages, and there are many challenges that need to be addressed before it can be implemented nationwide</p>
     </div>
+    </>
   )
 }
 
